d1dbc02aff4af0dea2934790f621114dce844361
Riikka Räty <48516441+riiraty@users.noreply.github.com>
2020-02-13 16:13:59 +0200

    tehtava 4.2

--- index.js
+++ index.js
@@ -1,67 +1,10 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const Blog = require('./models/blog')
+const app = require('./app') 
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-app.use(cors())
-app.use(express.json())
+const server = http.createServer(app)
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-app.use(requestLogger)
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello Blogilista!</h1>')
-})
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs.map(blog => blog.toJSON()))
-    })
-})
-
-app.post('/api/blogs', (request, response, next) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-      console.log('blog saved!')
-    })
-    .catch(error => next(error))
-})
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  // if (error.name === 'CastError' && error.kind == 'ObjectId') {
-  //   return response.status(400).send({ error: 'malformatted id' })
-  // } else if (error.name === 'ValidationError') {
-  //   return response.status(400).json({ error: error.message })
-  // }
-
-  next(error)
-}
-
-app.use(errorHandler)
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
