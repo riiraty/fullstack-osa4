da8533956d2942a106cc90056f28919eab16b7be
Riikka Räty <48516441+riiraty@users.noreply.github.com>
2020-02-14 19:07:02 +0200

    tehtavat 4.3-4.6

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const mongoose = require('mongoose')
 
 logger.info('connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
-  .then(result => {
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
---
+++ utils/list_helper.js
@@ -0,0 +1,66 @@
+const _ = require('lodash')
+
+const dummy = (blogs) => {
+  blogs.concat('dummy')
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const reducer = (favorite, item) => {
+    return (favorite.likes > item.likes)
+      ? favorite
+      : item
+  }
+
+  const favorite = blogs.length === 0
+    ? {}
+    : blogs.reduce(reducer, blogs[0])
+
+  const formattedFavorite = {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+
+  return formattedFavorite
+}
+
+const mostBlogs = (blogs) => {
+
+  const nameArray = blogs.map(blog => blog.author)
+
+  const mostCommon = _.head(_(nameArray)
+    .countBy()
+    .entries()
+    .maxBy(_.last))
+
+  const amountOfBlogs = _.remove(nameArray, (n) => {
+    return n === mostCommon
+  }).length
+
+  const mostBlogs = {
+    author: mostCommon,
+    blogs: amountOfBlogs
+  }
+
+  return blogs.length === 0
+    ? {}
+    : mostBlogs
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
+}
